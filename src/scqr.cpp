a000dd5214d0a929a6a32c26ab9ae056f9b2b720
Xiaoou Pan <xip024@ucsd.edu>
2020-08-13 23:19:27 -0700

    Code for scqr with Gaussian kernel

--- src/scqr.cpp
+++ src/scqr.cpp
@@ -14,6 +14,11 @@ double mad(const arma::vec& x) {
 }
 
 // [[Rcpp::export]]
+double getH(const double tau) {
+  return -std::log(1 - tau);
+}
+
+// [[Rcpp::export]]
 void updateHuber(const arma::mat& Z, const arma::vec& res, arma::vec& der, arma::vec& grad, const int n, const double tau, const double n1) {
   for (int i = 0; i < n; i++) {
     double cur = res(i);
@@ -66,8 +71,9 @@ arma::mat standardize(arma::mat X, const arma::rowvec& mx, const arma::vec& sx, 
 }
 
 // [[Rcpp::export]]
-void updateGauss(const arma::mat& Z, const arma::vec& res, arma::vec& der, arma::vec& grad, const double tau, const double n1, const double h1) {
-  der = arma::normcdf(-res * h1) - tau;
+void updateGauss(const arma::mat& Z, const arma::uvec& censor, const arma::vec& res, arma::vec& accu, arma::vec& der, arma::vec& grad, 
+                 const double n1, const double h1) {
+  der = censor % arma::normcdf(res * h1) - accu;
   grad = n1 * Z.t() * der;
 }
 
@@ -122,31 +128,21 @@ void updateTrian(const arma::mat& Z, const arma::vec& res, arma::vec& der, arma:
 }
 
 // [[Rcpp::export]]
-Rcpp::List smqrGauss(const arma::mat& X, arma::vec Y, const double tau = 0.5, double h = 0.0, const double constTau = 1.345, 
-                     const double tol = 0.0001, const int iteMax = 5000) {
-  const int n = X.n_rows;
-  const int p = X.n_cols;
-  if (h <= 0.05) {
-    h = std::max(std::pow((std::log(n) + p) / n, 0.4), 0.05);
-  }
-  const double n1 = 1.0 / n;
-  const double h1 = 1.0 / h;
-  arma::rowvec mx = arma::mean(X, 0);
-  arma::vec sx = arma::stddev(X, 0, 0).t();
-  arma::mat Z = arma::join_rows(arma::ones(n), standardize(X, mx, sx, p));
-  double my = arma::mean(Y);
-  Y -= my;
-  arma::vec der(n);
+arma::vec step0Gauss(const arma::mat& Z, const arma::vec& Y, const arma::uvec& censor, arma::vec& accu, const double tau, const double h, 
+                     const int n, const int p, const double n1, const double h1, const double constTau = 1.345, const double tol = 0.0001, 
+                     const int iteMax = 5000) {
   arma::vec gradOld(p + 1), gradNew(p + 1);
+  arma::vec der(n);
   arma::vec beta = huberReg(Z, Y, der, gradOld, gradNew, n, p, n1, tol, constTau, iteMax);
   arma::vec quant = {tau};
   beta(0) = arma::as_scalar(arma::quantile(Y - Z.cols(1, p) * beta.rows(1, p), quant));
-  arma::vec res = Y - Z * beta;
-  updateGauss(Z, res, der, gradOld, tau, n1, h1);
+  arma::vec res = Z * beta - Y;
+  accu = tau * arma::ones(n);
+  updateGauss(Z, censor, res, accu, der, gradOld, n1, h1);
   beta -= gradOld;
   arma::vec betaDiff = -gradOld;
-  res -= Z * betaDiff;
-  updateGauss(Z, res, der, gradNew, tau, n1, h1);
+  res += Z * betaDiff;
+  updateGauss(Z, censor, res, accu, der, gradNew, n1, h1);
   arma::vec gradDiff = gradNew - gradOld;
   int ite = 1;
   while (arma::norm(gradNew, "inf") > tol && ite <= iteMax) {
@@ -160,12 +156,71 @@ Rcpp::List smqrGauss(const arma::mat& X, arma::vec Y, const double tau = 0.5, do
     gradOld = gradNew;
     betaDiff = -alpha * gradNew;
     beta += betaDiff;
-    res -= Z * betaDiff;
-    updateGauss(Z, res, der, gradNew, tau, n1, h1);
+    res += Z * betaDiff;
+    updateGauss(Z, censor, res, accu, der, gradNew, n1, h1);
     gradDiff = gradNew - gradOld;
     ite++;
   }
+  return beta;
+}
+
+// [[Rcpp::export]]
+arma::vec stepkGauss(const arma::mat& Z, const arma::vec& Y, const arma::uvec& censor, arma::vec& beta, arma::vec& accu, const arma::vec& tauSeq, 
+                     const int k, const double h, const int n, const int p, const double n1, const double h1, const double tol = 0.0001, 
+                     const int iteMax = 5000) {
+  arma::vec gradOld(p + 1), gradNew(p + 1);
+  arma::vec der(n);
+  arma::vec res = Z * beta - Y;
+  accu += (Y >= Z * beta) % ((getH(tauSeq(k)) - getH(tauSeq(k - 1))) * arma::ones(n));
+  updateGauss(Z, censor, res, accu, der, gradOld, n1, h1);
+  beta -= gradOld;
+  arma::vec betaDiff = -gradOld;
+  res += Z * betaDiff;
+  updateGauss(Z, censor, res, accu, der, gradNew, n1, h1);
+  arma::vec gradDiff = gradNew - gradOld;
+  int ite = 1;
+  while (arma::norm(gradNew, "inf") > tol && ite <= iteMax) {
+    double alpha = 1.0;
+    double cross = arma::as_scalar(betaDiff.t() * gradDiff);
+    if (cross > 0) {
+      double a1 = cross / arma::as_scalar(gradDiff.t() * gradDiff);
+      double a2 = arma::as_scalar(betaDiff.t() * betaDiff) / cross;
+      alpha = std::min(std::min(a1, a2), 100.0);
+    }
+    gradOld = gradNew;
+    betaDiff = -alpha * gradNew;
+    beta += betaDiff;
+    res += Z * betaDiff;
+    updateGauss(Z, censor, res, accu, der, gradNew, n1, h1);
+    gradDiff = gradNew - gradOld;
+    ite++;
+  }
+  return beta;
+}
+
+// [[Rcpp::export]]
+Rcpp::List scqrGauss(const arma::mat& X, arma::vec Y, const arma::uvec& censor, const arma::vec& tauSeq, double h = 0.0, const double constTau = 1.345, 
+                     const double tol = 0.0001, const int iteMax = 5000) {
+  const int n = X.n_rows;
+  const int p = X.n_cols;
+  const int m = tauSeq.size();
+  if (h <= 0.05) {
+    h = std::max(std::pow((std::log(n) + p) / n, 0.4), 0.05);
+  }
+  const double n1 = 1.0 / n;
+  const double h1 = 1.0 / h;
+  arma::rowvec mx = arma::mean(X, 0);
+  arma::vec sx = arma::stddev(X, 0, 0).t();
+  arma::mat Z = arma::join_rows(arma::ones(n), standardize(X, mx, sx, p));
+  double my = arma::mean(Y);
+  Y -= my;
+  arma::vec accu(n);
+  arma::vec beta = step0Gauss(Z, Y, censor, accu, tauSeq(0), h, n, p, n1, h1, constTau, tol, iteMax);
+  for (int k = 1; k <= m; k++) {
+    beta = stepkGauss(Z, Y, censor, beta, accu, tauSeq, k, h, n, p, n1, h1, tol, iteMax);
+  }
   beta.rows(1, p) /= sx;
   beta(0) += my - arma::as_scalar(mx * beta.rows(1, p));
-  return Rcpp::List::create(Rcpp::Named("coeff") = beta, Rcpp::Named("ite") = ite, Rcpp::Named("residual") = res, Rcpp::Named("bandwidth") = h);
+  return Rcpp::List::create(Rcpp::Named("coeff") = beta, Rcpp::Named("bandwidth") = h);
 }
+ 
