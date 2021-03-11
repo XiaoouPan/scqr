9ecca58c9e3a1bea81a69ab88b50f33b3338647c
Xiaoou Pan <xip024@ucsd.edu>
2021-03-10 21:00:44 -0800

    Quantile effects

--- R/simulation.R
+++ R/simulation.R
@@ -10,13 +10,28 @@ library(matrixStats)
 library(survival)
 library(tikzDevice)
 
+getSigma = function(p) {
+  sig = diag(p)
+  for (i in 1:(p - 1)) {
+    for (j in (i + 1):p) {
+      sig[i, j] = sig[j, i] = 0.5^(j - i)
+    }
+  }
+  return (sig)
+}
 
-#### Quantile process with fixed scale
+estError = function(betahat, beta, tauSeq) {
+  nTau = length(tauSeq)
+  
+}
+
+
+#### Quantile process with fixed scale, hard to visualize
 n = 5000
-p = n / 40
-M = 1
-tauSeq = seq(0.1, 0.9, by = 0.05)
-grid = seq(0.1, 0.95, by = 0.05)
+p = n / 50
+M = 100
+tauSeq = seq(0.05, 0.95, by = 0.05)
+grid = seq(0.05, 0.95, by = 0.05)
 nTau = length(tauSeq)
 beta0 = qt(tauSeq, 2)
 coef1 = itcp1 = matrix(0, M, nTau)
@@ -24,13 +39,18 @@ coef2 = itcp2 = matrix(0, M, nTau)
 time = matrix(0, 2, M)
 prop = rep(0, M)
 
+pb = txtProgressBar(style = 3)
 for (i in 1:M) {
   set.seed(i)
-  X = matrix(rnorm(n * p), n, p)
-  beta = runif(p, 1, 2)
+  Sigma = getSigma(p)
+  #X = sqrt(12) * draw.d.variate.uniform(n, p, Sigma) - sqrt(3)
+  X = mvrnorm(n, rep(0, p), Sigma)
+  beta = runif(p, -2, 2)
+  betaMat = rbind(beta0, matrix(beta, p, nTau))
   err = rt(n, 2)
   logT = X %*% beta + err
-  logC = rnorm(n, 5, 4)
+  w = sample(1:3, n, prob = c(1/3, 1/3, 1/3), replace = TRUE)
+  logC = (w == 1) * rnorm(n, 0, 4) + (w == 2) * rnorm(n, 5, 1) + (w == 3) * rnorm(n, 10, 0.5)
   censor = logT <= logC
   prop[i] = 1 - sum(censor) / n
   Y = pmin(logT, logC)
@@ -40,23 +60,34 @@ for (i in 1:M) {
   list = scqrGauss(X, Y, censor, tauSeq)
   end = Sys.time()
   time[1, i] = as.numeric(difftime(end, start, units = "secs"))
-  coef1[i, ] = colMeans((list$coeff[-1, ] - beta)^2)
-  itcp1[i, ] = colMeans(rbind((list$coeff[1, ] - beta0)^2, (list$coeff[-1, ] - beta)^2))
+  coef1[i, ] = sqrt(colSums((list$coeff - betaMat)^2))
+  itcp1[i, ] = list$coeff[1, ]
   
   start = Sys.time()
   list = crq(response ~ X, method = "PengHuang", grid = grid)
   end = Sys.time()
   time[2, i] = as.numeric(difftime(end, start, units = "secs"))
-  coef2[i, ] = colMeans((list$sol[3:(p + 2), ] - beta)^2)
-  itcp2[i, ] = colMeans(rbind((list$sol[2, ] - beta0)^2, (list$sol[3:(p + 2), ] - beta)^2))
+  tt = ncol(list$sol)
+  coef2[i, 1:tt] = sqrt(colSums((list$sol[2:(p + 2), ] - betaMat[, 1:tt])^2))
+  itcp2[i, 1:tt] = list$sol[2, ]
   
-  setTxtProgressBar(pb, ((j - 1) * M + i) / (l * M))
+  setTxtProgressBar(pb, i / M)
 }
 
+time
+sum(coef2[, nTau - 1] == 0)
+est = rbind(colMeans(coef1), colMeans(coef2))
+plot(tauSeq[-nTau], est[2, -nTau], type = "b", pch = 1, lwd = 5, cex = 1, col = "red", axes = FALSE, xlim = c(0, 1), 
+     ylim = c(min(pretty(range(est))), max(pretty(range(est)))), xlab = "", ylab = "")
+lines(tauSeq, est[1, ], type = "b", pch = 2, lwd = 5, cex = 1, col = "blue")
+est = rbind(colMeans(itcp1), colMeans(itcp2))
+plot(tauSeq[-nTau], est[2, -nTau], type = "b", pch = 1, lwd = 5, cex = 1, col = "red", axes = FALSE, xlim = c(0, 1), 
+     ylim = c(min(pretty(range(est))), max(pretty(range(est)))), xlab = "", ylab = "")
+lines(tauSeq, est[1, ], type = "b", pch = 2, lwd = 5, cex = 1, col = "blue")
+lines(tauSeq, beta0, type = "l", lwd = 5, cex = 1)
 
 
-
-#### Growing dimension
+#### Growing dimension and sample size
 nseq = seq(2000, 6000, by = 2000)
 pseq = floor(nseq / 40)
 l = length(nseq)
