2e9e9465426da8f256f022847fc9098f5dd37bde
Xiaoou Pan <xip024@ucsd.edu>
2021-03-13 00:34:53 -0800

    Update simulation results

--- R/simulation.R
+++ R/simulation.R
@@ -150,7 +150,6 @@ beta0 = qt(tauSeq, 2)
 M = 20
 coef1 = coef2 = time1 = time2 = prop = matrix(0, M, l)
 
-## Homo 
 pb = txtProgressBar(style = 3)
 for (j in 1:l) {
   n = nseq[j]
@@ -160,9 +159,15 @@ for (j in 1:l) {
     Sigma = getSigma(p)
     X = mvrnorm(n, rep(0, p), Sigma)
     err = rt(n, 2)
-    beta = runif(p, -2, 2)
-    betaMat = rbind(beta0, matrix(beta, p, nTau))
-    logT = X %*% beta + err
+    ## Homo 
+    #beta = runif(p, -2, 2)
+    #betaMat = rbind(beta0, matrix(beta, p, nTau))
+    #logT = X %*% beta + err
+    ## Hetero
+    X[, 1] = abs(X[, 1])
+    beta = runif(p - 1, -2, 2)
+    betaMat = rbind(rep(0, nTau), beta0, matrix(beta, p - 1, nTau))
+    logT = X[, 1] * err + X[, -1] %*% beta
     w = sample(1:3, n, prob = c(1/3, 1/3, 1/3), replace = TRUE)
     logC = (w == 1) * rnorm(n, 0, 4) + (w == 2) * rnorm(n, 5, 1) + (w == 3) * rnorm(n, 10, 0.5)
     censor = logT <= logC
@@ -191,11 +196,11 @@ for (j in 1:l) {
 
 
 
-#write.csv(prop, "~/Dropbox/Conquer/censoredQR/Code/Simulation/Growing/prop.csv")
-#write.csv(time1, "~/Dropbox/Conquer/censoredQR/Code/Simulation/Growing/time1.csv")
-#write.csv(time2, "~/Dropbox/Conquer/censoredQR/Code/Simulation/Growing/time2.csv")
-#write.csv(coef1, "~/Dropbox/Conquer/censoredQR/Code/Simulation/Growing/coef1.csv")
-#write.csv(coef2, "~/Dropbox/Conquer/censoredQR/Code/Simulation/Growing/coef2.csv")
+write.csv(prop, "~/Dropbox/Conquer/censoredQR/Code/Simulation/Growing/prop.csv")
+write.csv(time1, "~/Dropbox/Conquer/censoredQR/Code/Simulation/Growing/time1.csv")
+write.csv(time2, "~/Dropbox/Conquer/censoredQR/Code/Simulation/Growing/time2.csv")
+write.csv(coef1, "~/Dropbox/Conquer/censoredQR/Code/Simulation/Growing/coef1.csv")
+write.csv(coef2, "~/Dropbox/Conquer/censoredQR/Code/Simulation/Growing/coef2.csv")
 #time = as.matrix(read.csv("~/Dropbox/Conquer/censoredQR/Code/Simulation/time.csv"))[, -1]
 #coef = as.matrix(read.csv("~/Dropbox/Conquer/censoredQR/Code/Simulation/coef.csv"))[, -1]
 
@@ -207,9 +212,9 @@ plot(nseq, coef[2, ], type = "b", pch = 1, lwd = 5, cex = 1, col = "red", axes =
      ylim = c(min(pretty(range(coef))), max(pretty(range(coef)))), xlab = "", ylab = "")
 lines(nseq, coef[1, ], type = "b", pch = 2, lwd = 5, cex = 1, col = "blue")
 axis(1, nseq[c(2, 4, 6, 8, 10)], nseq[c(2, 4, 6, 8, 10)] / 1000, tick = TRUE, line = 0, cex.axis = 1.5)
-axis(2, seq(0.4, 1.2, by = 0.2), line = 0, cex.axis = 1.5)
+axis(2, pretty(range(coef)), line = 0, cex.axis = 1.5)
 box()
-abline(h = seq(0.4, 1.2, by = 0.2), v = nseq[c(2, 4, 6, 8, 10)], col = "gray", lty = 2)
+abline(h = pretty(range(coef)), v = nseq[c(2, 4, 6, 8, 10)], col = "gray", lty = 2)
 #color = c("red", "blue")
 #labels = c("\\texttt{censored QR}", "\\texttt{proposed method}")
 #pch = c(1, 2)
@@ -220,7 +225,7 @@ dev.off()
 tools::texi2dvi("plot.tex", pdf = T)
 
 ## Time plot
-tikz("plot.tex", standAlone = TRUE, width = 6, height = 6)
+tikz("plot.tex", standAlone = TRUE, width = 5, height = 5)
 plot(nseq, time[2, ], type = "b", pch = 1, cex = 1, lwd = 5, col = "red", axes = FALSE, xlim = c(1000, 20000), 
      ylim = c(min(pretty(range(time))), max(pretty(range(time)))), xlab = "", ylab = "")
 lines(nseq, time[1, ], type = "b", pch = 2, cex = 1, lwd = 5, col = "blue")
@@ -231,7 +236,7 @@ abline(h = pretty(range(time)), v = nseq[c(2, 4, 6, 8, 10)], col = "gray", lty =
 color = c("red", "blue")
 labels = c("CQR", "smoothed CQR")
 pch = c(1, 2)
-legend("topleft", labels, col = color, pch = pch, lwd = 3, cex = 2, box.lwd = 1, bg = "white")
+legend("topleft", labels, col = color, pch = pch, lwd = 3, cex = 1.5, box.lwd = 1, bg = "white")
 title(xlab = "Sample size (in thousands)", line = 2.5, cex.lab = 1.8)
 title(ylab = "Time elapsed (in seconds)", line = 2.5, cex.lab = 1.8)
 dev.off()
