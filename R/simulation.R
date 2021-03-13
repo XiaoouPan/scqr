bb47f61d3b39c167fbf3411083515a7ac0eb4c45
Xiaoou Pan <xip024@ucsd.edu>
2021-03-12 17:13:50 -0800

    Simulation graphs

--- R/simulation.R
+++ R/simulation.R
@@ -181,7 +181,9 @@ for (j in 1:l) {
     end = Sys.time()
     time2[i, j] = as.numeric(difftime(end, start, units = "secs"))
     tt = ncol(list$sol)
-    coef2[i, j] = estError(list$sol[2:(p + 2), ], betaMat[, 1:tt], tauSeq)
+    if (tt >= nTau - 1) {
+      coef2[i, j] = estError(list$sol[2:(p + 2), ], betaMat[, 1:tt], tauSeq)
+    }
     
     setTxtProgressBar(pb, ((j - 1) * M + i) / (l * M))
   }
@@ -189,41 +191,49 @@ for (j in 1:l) {
 
 
 
-
-prop = prop / M
-time = time / M
-coef = coef / M
-#write.csv(prop, "~/Dropbox/Conquer/censoredQR/Code/Simulation/prop.csv")
-#write.csv(time, "~/Dropbox/Conquer/censoredQR/Code/Simulation/time.csv")
-#write.csv(coef, "~/Dropbox/Conquer/censoredQR/Code/Simulation/coef.csv")
+#write.csv(prop, "~/Dropbox/Conquer/censoredQR/Code/Simulation/Growing/prop.csv")
+#write.csv(time1, "~/Dropbox/Conquer/censoredQR/Code/Simulation/Growing/time1.csv")
+#write.csv(time2, "~/Dropbox/Conquer/censoredQR/Code/Simulation/Growing/time2.csv")
+#write.csv(coef1, "~/Dropbox/Conquer/censoredQR/Code/Simulation/Growing/coef1.csv")
+#write.csv(coef2, "~/Dropbox/Conquer/censoredQR/Code/Simulation/Growing/coef2.csv")
 #time = as.matrix(read.csv("~/Dropbox/Conquer/censoredQR/Code/Simulation/time.csv"))[, -1]
 #coef = as.matrix(read.csv("~/Dropbox/Conquer/censoredQR/Code/Simulation/coef.csv"))[, -1]
 
+time = rbind(colMeans(time1), colMeans(time2))
+coef = rbind(colMeans(coef1), colMeans(coef2))
 setwd("~/Dropbox/Conquer/censoredQR/Code")
-tikz("plot.tex", standAlone = TRUE, width = 6, height = 6)
-plot(coef[2, ], type = "b", pch = 1, lwd = 5, cex = 1, col = "red", axes = FALSE, ylim = c(min(pretty(range(coef))), max(pretty(range(coef)))), xlab = "", ylab = "")
-lines(coef[1, ], type = "b", pch = 2, lwd = 5, cex = 1, col = "blue")
-grid(col = "grey", lty = 2)
-color = c("red", "blue")
-labels = c("\\texttt{censored QR}", "\\texttt{proposed method}")
-pch = c(1, 2)
-legend(7, 0.014, labels, col = color, pch = pch, lwd = 5, cex = 2, box.lwd = 1, bg = "white")
-axis(1, c(1, seq(5, 20, by = 5)), nseq[c(1, seq(5, 20, by = 5))] / 1000, tick = TRUE, line = -1, cex.axis = 1.8)
-axis(2, pretty(range(coef)), line = -0.7, cex.axis = 1.8)
-title(xlab = "Sample Size (in thousands)", line = 1.5, cex.lab = 2)
-title(ylab = "Estimation Error", line = 1.7, cex.lab = 2)
+tikz("plot.tex", standAlone = TRUE, width = 5, height = 5)
+plot(nseq, coef[2, ], type = "b", pch = 1, lwd = 5, cex = 1, col = "red", axes = FALSE, xlim = c(1000, 20000), 
+     ylim = c(min(pretty(range(coef))), max(pretty(range(coef)))), xlab = "", ylab = "")
+lines(nseq, coef[1, ], type = "b", pch = 2, lwd = 5, cex = 1, col = "blue")
+axis(1, nseq[c(2, 4, 6, 8, 10)], nseq[c(2, 4, 6, 8, 10)] / 1000, tick = TRUE, line = 0, cex.axis = 1.5)
+axis(2, seq(0.4, 1.2, by = 0.2), line = 0, cex.axis = 1.5)
+box()
+abline(h = seq(0.4, 1.2, by = 0.2), v = nseq[c(2, 4, 6, 8, 10)], col = "gray", lty = 2)
+#color = c("red", "blue")
+#labels = c("\\texttt{censored QR}", "\\texttt{proposed method}")
+#pch = c(1, 2)
+#legend(7, 0.014, labels, col = color, pch = pch, lwd = 5, cex = 2, box.lwd = 1, bg = "white")
+title(xlab = "Sample size (in thousands)", line = 2.5, cex.lab = 1.8)
+title(ylab = "Global estimation error", line = 2.5, cex.lab = 1.8)
 dev.off()
 tools::texi2dvi("plot.tex", pdf = T)
 
 ## Time plot
 tikz("plot.tex", standAlone = TRUE, width = 6, height = 6)
-plot(time[2, ], type = "b", pch = 1, cex = 1, lwd = 5, col = "red", axes = FALSE, ylim = c(min(pretty(range(time))), max(pretty(range(time)))), xlab = "", ylab = "")
-lines(time[1, ], type = "b", pch = 2, cex = 1, lwd = 5, col = "blue")
-grid(col = "grey", lty = 2)
-axis(1, c(1, seq(5, 20, by = 5)), nseq[c(1, seq(5, 20, by = 5))] / 1000, tick = TRUE, line = -0.6, cex.axis = 1.8)
-axis(2, pretty(range(time)), line = -0.7, cex.axis = 1.8)
-title(xlab = "Sample Size (in thousands)", line = 2, cex.lab = 2)
-title(ylab = "Time Elapsed", line = 1.7, cex.lab = 2)
+plot(nseq, time[2, ], type = "b", pch = 1, cex = 1, lwd = 5, col = "red", axes = FALSE, xlim = c(1000, 20000), 
+     ylim = c(min(pretty(range(time))), max(pretty(range(time)))), xlab = "", ylab = "")
+lines(nseq, time[1, ], type = "b", pch = 2, cex = 1, lwd = 5, col = "blue")
+axis(1, nseq[c(2, 4, 6, 8, 10)], nseq[c(2, 4, 6, 8, 10)] / 1000, tick = TRUE, line = 0, cex.axis = 1.5)
+axis(2, pretty(range(time)), line = 0, cex.axis = 1.5)
+box()
+abline(h = pretty(range(time)), v = nseq[c(2, 4, 6, 8, 10)], col = "gray", lty = 2)
+color = c("red", "blue")
+labels = c("CQR", "smoothed CQR")
+pch = c(1, 2)
+legend("topleft", labels, col = color, pch = pch, lwd = 3, cex = 2, box.lwd = 1, bg = "white")
+title(xlab = "Sample size (in thousands)", line = 2.5, cex.lab = 1.8)
+title(ylab = "Time elapsed (in seconds)", line = 2.5, cex.lab = 1.8)
 dev.off()
 tools::texi2dvi("plot.tex", pdf = T)
 
