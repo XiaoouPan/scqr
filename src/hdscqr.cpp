9cbb261f050a036e1ed63d91c209a1b5863a63fc
XiaoouPan <xip024@ucsd.edu>
2022-03-27 14:18:21 -0700

    cross-validation for increment

--- src/hdscqr.cpp
+++ src/hdscqr.cpp
@@ -811,6 +811,72 @@ Rcpp::List cvSqrLassoGrow(const arma::mat& X, const arma::vec& censor, arma::vec
 }
 
 // [[Rcpp::export]]
+Rcpp::List cvSqrLassoIncr(const arma::mat& X, const arma::vec& censor, arma::vec Y, const arma::vec& lambdaSeq, const arma::vec& incrSeq, const arma::vec& folds, 
+                          const arma::vec& tauSeq, const int kfolds, const double h, const double phi0 = 0.1, const double gamma = 1.2, 
+                          const double epsilon = 0.01, const int iteMax = 500) {
+  const int n = X.n_rows, p = X.n_cols, nlambda = lambdaSeq.size(), nincr = incrSeq.size();
+  const int m = tauSeq.size();
+  const double h1 = 1.0 / h, h2 = 1.0 / (h * h);
+  arma::vec betaHat(p + 1);
+  arma::mat betaProc(p + 1, m);
+  arma::vec mse = arma::zeros(nlambda, nincr);
+  arma::rowvec mx = arma::mean(X, 0);
+  arma::vec sx1 = 1.0 / arma::stddev(X, 0, 0).t();
+  arma::mat Z = arma::join_rows(arma::ones(n), standardize(X, mx, sx1, p));
+  double my = arma::mean(Y);
+  Y -= my;
+  arma::vec HSeq = getH(tauSeq);
+  mse = arma::zeros(nlambda);
+  for (int j = 1; j <= kfolds; j++) {
+    arma::uvec idx = arma::find(folds == j);
+    arma::uvec idxComp = arma::find(folds != j);
+    int nTrain = idxComp.size();
+    int nTest = idx.size();
+    double n1Train = 1.0 / idxComp.size();
+    arma::mat trainZ = Z.rows(idxComp), testZ = Z.rows(idx);
+    arma::vec trainY = Y.rows(idxComp), testY = Y.rows(idx);
+    arma::vec trainCensor = censor.rows(idxComp), testCensor = censor.rows(idx);
+    for (int i = 0; i < nlambda; i++) {
+      double lambda = lambdaSeq(i);
+      for (int l = 0; l < nincr; l++) {
+        double incr = incrSeq(l);
+        arma::vec trainAccu = tauSeq(0) * arma::ones(nTrain);
+        betaHat = sqr0Lasso(trainZ, trainCensor, trainY, lambda, trainAccu, tauSeq(0), p, n1Train, h, h1, h2, phi0, gamma, epsilon, iteMax);
+        betaProc.col(0) = betaHat;
+        for (int k = 1; k < m; k++) {
+          arma::vec trainRes = trainY - trainZ * betaHat;
+          trainAccu += arma::normcdf(trainRes * h1) * (HSeq(k) - HSeq(k - 1));
+          betaHat = sqrkLasso(trainZ, trainCensor, trainY, lambda + incr * k, trainAccu, betaHat, p, n1Train, h, h1, h2, phi0, gamma, epsilon, iteMax);
+          betaProc.col(k) =  betaHat;
+        }
+        mse(i, l) += calRes(testZ, testCensor, testY, betaProc, tauSeq, HSeq, m, nTest);
+      }
+    }
+  }
+  arma::uword cvIdx = arma::index_min(mse);
+  arma::vec accu = tauSeq(0) * arma::ones(n);
+  double lambda = lambdaSeq((int)cvIdx % nlambda);
+  double incr = incrSeq((int)cvIdx / nlambda);
+  betaHat = sqr0Lasso(Z, censor, Y, lambda, accu, tauSeq(0), p, 1.0 / n, h, h1, h2, phi0, gamma, epsilon, iteMax);
+  betaProc.col(0) = betaHat;
+  for (int k = 1; k < m; k++) {
+    arma::vec res = Y - Z * betaHat;
+    accu += arma::normcdf(res * h1) * (HSeq(k) - HSeq(k - 1));
+    betaHat = sqrkLasso(Z, censor, Y, lambda + incr * k, accu, betaHat, p, 1.0 / n, h, h1, h2, phi0, gamma, epsilon, iteMax);
+    betaProc.col(k) =  betaHat;
+  }
+  betaProc.rows(1, p).each_col() %= sx1;
+  betaProc.row(0) += my - mx * betaProc.rows(1, p);
+  return Rcpp::List::create(Rcpp::Named("beta") = betaProc, Rcpp::Named("lambda0") = lambda, cpp::Named("incr0") = incr);
+}
+
+// [[Rcpp::export]]
+int exam(arma::mat A) {
+  arma::uword cvIdx = A.index_min();
+  return (int)cvIdx % 3;
+}
+  
+// [[Rcpp::export]]
 arma::mat SqrScad(const arma::mat& X, const arma::vec& censor, arma::vec Y, const double lambda, const arma::vec& tauSeq, const double h, 
                   const double phi0 = 0.1, const double gamma = 1.2, const double epsilon = 0.01, const int iteMax = 500) {
   const int n = X.n_rows, p = X.n_cols;
